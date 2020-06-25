bc6eeb3b2d43f6e9fe49b654bcdfffe563c90ffb
xSuperMu <muhammadelsayed96@yahoo.com>
2020-06-25 22:50:11 +0200

    all good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DeviceOrientation } from '@ionic-native/device-orientation/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +18,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    DeviceOrientation,
+    Geolocation
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation/ngx';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public data: DeviceOrientationCompassHeading = null;
+  public deviceCurrentLocation: Geoposition = null;
+  private kaabaLocation: {latitude:number,longitude:number} = {latitude: 21.42276, longitude: 39.8256687};
+  public qiblaLocation = 0;
 
+  constructor(private deviceOrientation: DeviceOrientation, private geolocation: Geolocation) {
+
+    this.deviceOrientation.watchHeading().subscribe((res: DeviceOrientationCompassHeading) => {
+      this.data = res;
+
+      if (!!this.deviceCurrentLocation) {
+        const currentQibla = res.magneticHeading - this.getQiblaPosition();
+        this.qiblaLocation = currentQibla > 360 ? currentQibla%360 : currentQibla;
+      }
+    });
+    
+    this.geolocation.watchPosition().subscribe((res) => {
+      this.deviceCurrentLocation = res;
+  });
+  }
+
+  getQiblaPosition() {
+    // Convert degrees to radians
+    const curLocationLat = this.convertDegreesToRadians(this.deviceCurrentLocation.coords.latitude);
+    const curLocationLng = this.convertDegreesToRadians(this.deviceCurrentLocation.coords.longitude);
+    const qiblaLocationLat = this.convertDegreesToRadians(this.kaabaLocation.latitude);
+    const qiblaLocationLng = this.convertDegreesToRadians(this.kaabaLocation.longitude);
+
+    // Use Basic Spherical Trigonometric Formula
+    return this.convertRadiansToDegrees(
+      Math.atan2(
+        Math.sin(qiblaLocationLng-curLocationLng),
+        (Math.cos(curLocationLat) * Math.tan(qiblaLocationLat) - Math.sin(curLocationLat) * Math.cos(qiblaLocationLng - curLocationLng))
+      )
+    );
+  }
+  convertRadiansToDegrees(radians: number) {
+    return radians * 180 / Math.PI;
+  }
+
+  convertDegreesToRadians(degrees: number) {
+    return degrees * Math.PI / 180;
+  }
 }
